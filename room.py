19b3dc469978b7281f3a54ec42b459b0436495f5
Aqueno Nirasmi <aqueno10@uw.edu>
2023-12-11 18:14:25 -0800

    added current_room

--- room.py
+++ room.py
@@ -21,6 +21,7 @@ class Room:
         self.__inheritance_pillar = False
         self.__polymorphism_pillar = False
         self.__empty_room = False
+        self.__current_room = False
 
     # getter methods
     def get_healing_potion(self):
@@ -91,6 +92,10 @@ class Room:
         """ gets exit boolean value """
         return self.__exit
 
+    def get_current_room(self):
+        """ gets current_room boolean value """
+        return self.__current_room
+
     # setter methods
 
     def set_healing_potion(self, add_potion):
@@ -140,13 +145,13 @@ class Room:
         """ setting polymorphism_pillar """
         self.__polymorphism_pillar = polymorphism_pillar
 
-    def set_empty_room(self):
+    def set_empty_room(self, isEmpty):
         """ setting empty_room """
-        self.__empty_room = True
+        self.__empty_room = isEmpty
 
-    def set_entrance(self):
+    def set_entrance(self, entrance):
         """ setting entrance """
-        self.__entrance = True
+        self.__entrance = entrance
 
     def set_exit(self):
         """ setting exit """
@@ -163,6 +168,10 @@ class Room:
     def set_multiple_items(self, multiple_items):
         """ This method is for one or more items in a room """
         self.__multiple_items = multiple_items
+
+    def set_current_room(self, current_room):
+        """ setting current_room """
+        self.__current_room = current_room
 
     def can_enter(self):
         """ This method can be called if there is no impasse and if it is not visited """
@@ -194,6 +203,8 @@ class Room:
             layout += "M"
         elif self.__empty_room:
             layout += " "
+        elif self.__current_room:
+            layout += "#"
         elif self.__abstraction_pillar:
             layout += "A"
         elif self.__polymorphism_pillar:
